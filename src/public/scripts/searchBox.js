4e4aea0642749b54a2dbd0da3fa1e3cda0e67bb4
Gwilym Rutherford <gwilym.rutherford@yahoo.com>
2025-03-11 19:19:20 +0000

    Move all script to client
    Also added feature to have alias on either side of the search query so something like 'the beegeese yt' will search the beegeese on yoututbe

--- src/public/scripts/searchBox.js
+++ src/public/scripts/searchBox.js
@@ -3,15 +3,14 @@ let searchBox = document.getElementById("searchBox");
 fetch("config.json")
   .then(response => response.json())
   .then(config => {
+    searchBox.addEventListener("input", instantAlias(config));
+	searchBox.addEventListener("keyup", (event)=>{
+		if (event.key === 'Enter'){
+			console.log("triggered");
+			alias(config);
+		}
+	});
 
-    searchBox.addEventListener("input", () => {
-      let instantAlias = config.instantAliasInfo;
-      instantAlias.forEach((item) => {
-        if (item.alias === searchBox.value) {
-          window.location.href = item.url;
-        }
-      });
-    });
   })
   .catch(error => {
     console.error("Error loading config.json:", error);
@@ -23,14 +22,50 @@ document.addEventListener("keydown", () => {
     }
 }); 
 
+function instantAlias(config){
+  let instantAlias = config.instantAliasInfo;
+  instantAlias.forEach((item) => {
+    if (item.alias === searchBox.value) {
+      window.location.href = item.url;
+    }
+  });
+}
 
-// searchBox.addEventListener("input", ()=>{
-//     console.log("test");
-//     let instantAlias = config.instantAliasInfo;
-//     console.log("test");
-//     instantAlias.forEach((item)=>{
-//         if(item.alias == searchBox.textContent){
-//             window.location.href = item.url;
-//         }
-//     });
-// });
+function alias(config){
+	let query = searchBox.value;
+	let tokens = query.split(" ");
+	let tokenLength = tokens.length;
+	let alias = [tokens[0], tokens[tokenLength - 1]];
+
+	let found = false;
+	let url;
+	let aliasInfo = config.aliasInfo;
+	for(let item of aliasInfo){
+		if (item.alias === alias[0]){
+			url = item.url + tokens.slice(1).join(" ");;
+			found = true;
+			break;
+		}else if (item.alias == alias[1]){
+			url = item.url + tokens.slice(0, tokenLength - 1).join(" ");;
+			found = true;
+			break;
+		}
+
+	}
+
+	if(found){
+		window.location.href = url;
+	}else{
+		window.location.href = config.defaultURL + query;
+	}
+
+}
+
+
+function encodeString(query) {
+    if(typeof query === "string"){
+        return query.split(" ").join("+");
+    } else {
+        return "";
+    }
+}
--- src/routes/Alias.js
+++ src/routes/Alias.js
@@ -8,35 +8,28 @@ alias.get("/", (req,res)=>{
 });
 
 
-alias.post("/", (req,res)=>{
-    let query = req.body.query;
+// alias.post("/", (req,res)=>{
+//     let query = req.body.query;
     
-    // split query and find first section
-    let tokenized = query.split(" ");
-    let alias = tokenized[0];
-    let searchTerm = tokenized.slice(1).join("+");
+//     // split query and find first section
+//     let tokenized = query.split(" ");
+//     let alias = tokenized[0];
+//     let searchTerm = tokenized.slice(1).join("+");
 
-    let match = false;
+//     let match = false;
 
-    config.aliasInfo.forEach((block)=>{
-        if(block.alias == alias){
-            res.redirect(block.url + searchTerm);
-            match = true;
-            return;
-        }
-        match = false;
-    });
+//     config.aliasInfo.forEach((block)=>{
+//         if(block.alias == alias){
+//             res.redirect(block.url + searchTerm);
+//             match = true;
+//             return;
+//         }
+//         match = false;
+//     });
 
-    if(!match){
-        res.redirect(config.aliasInfo[0].url + encode(query));
-    }
+//     if(!match){
+//         res.redirect(config.aliasInfo[0].url + encode(query));
+//     }
     
-});
+// });
 
-function encode(query) {
-    if(typeof query === "string"){
-        return query.split(" ").join("+");
-    } else {
-        return "";
-    }
-}
